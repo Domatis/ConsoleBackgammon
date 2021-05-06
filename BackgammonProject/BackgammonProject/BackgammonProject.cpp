2c5a5d9930b7aa21ed1262b1ce0f2cdde512e4d8
olric93 <cem.bozkurt.3493@gmail.com>
2021-05-06 14:41:43 +0300

    -First Commit

---
+++ BackgammonProject/BackgammonProject/BackgammonProject.cpp
@@ -0,0 +1,11 @@
+﻿// BackgammonProject.cpp : Bu dosya 'main' işlevi içeriyor. Program yürütme orada başlayıp biter.
+//
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
+
+
